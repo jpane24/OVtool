f2c2f285aa3dea14e6e8a62d4e72eef4df073e5b
Pane, Joseph <josephp@rand.org>
2020-03-05 13:42:46 -0500

    small changes summary fn (using correct effect size)

--- R/summary.ov.R
+++ R/summary.ov.R
@@ -6,43 +6,43 @@ summary.ov <- function(r1, sig_level=0.05){
   raw_pval = r1$p_val[which(r1$es_grid<.000000001 & r1$es_grid>-.000000001),
                       which(r1$rho_grid==0)]
   # Effect Size cases
-  effect_size_text = case_when((raw_treat < 0 & all(temp$obs_cors$ES < 0)) |
-                                 raw_treat >= 0 & all(temp$obs_cors$ES >= 0) ~ "no sign changes",
-                               raw_treat < 0 & all(temp$obs_cors$ES >= 0) |
-                                 raw_treat >=0 & all(temp$obs_cors$ES < 0) ~ "all sign changes",
+  effect_size_text = case_when((raw_treat < 0 & all(temp$obs_cors$ESTRUE < 0)) |
+                                 raw_treat >= 0 & all(temp$obs_cors$ESTRUE >= 0) ~ "no sign changes",
+                               raw_treat < 0 & all(temp$obs_cors$ESTRUE >= 0) |
+                                 raw_treat >=0 & all(temp$obs_cors$ESTRUE < 0) ~ "all sign changes",
                                TRUE ~ "some sign changes")
 
   if(effect_size_text == "no sign changes"){
-    diff = diff(c(min(abs(temp$obs_cors$ES)), abs(raw_treat)))
+    diff = diff(c(min(abs(temp$obs_cors$ESTRUE)), abs(raw_treat)))
     percent_reduced = abs(round(diff/raw_treat*100))
     text = paste0("The sign of the estimated effect is expected to be robust to unobserved confounders that have the same strength of association with the treatment indicator and outcome that are seen in the observed confounders. In the most extreme observed case, the estimated effect size is reduced by ", percent_reduced, " percent.")
   } else if(effect_size_text == "all sign changes"){
     if(raw_treat < 0){
-      diff = diff(c(max(temp$obs_cors$ES), raw_treat))
+      diff = diff(c(max(temp$obs_cors$ESTRUE), raw_treat))
     } else{
-      diff = diff(c(min(temp$obs_cors$ES), raw_treat))
+      diff = diff(c(min(temp$obs_cors$ESTRUE), raw_treat))
     }
     percent_reduced = abs(round(diff/raw_treat*100))
     text = paste0("The sign of the estimated effect is *not* expected to be robust to unobserved confounders that have the same strength of association with the treatment indicator and outcome that are seen in any of the observed confounders. In the most extreme observed case, the estimated effect size is ", percent_reduced, " percent of the original, but in the opposite direction")
   } else if(effect_size_text == "some sign changes"){
     # calculate sign changes
     if(raw_treat < 0){
-      change = length(which(temp$obs_cors$ES >= 0))
-      nochange = length(which(temp$obs_cors$ES < 0))
+      change = length(which(temp$obs_cors$ESTRUE >= 0))
+      nochange = length(which(temp$obs_cors$ESTRUE < 0))
       total = nrow(temp$obs_cors)
-      changes = temp$obs_cors$cov[which(temp$obs_cors$ES >=0)]
+      changes = temp$obs_cors$cov[which(temp$obs_cors$ESTRUE >=0)]
     } else{
-      change = length(which(temp$obs_cors$ES < 0))
-      nochange = length(which(temp$obs_cors$ES >= 0))
+      change = length(which(temp$obs_cors$ESTRUE < 0))
+      nochange = length(which(temp$obs_cors$ESTRUE >= 0))
       total = nrow(temp$obs_cors)
-      changes = temp$obs_cors$cov[which(temp$obs_cors$ES < 0)]
+      changes = temp$obs_cors$cov[which(temp$obs_cors$ESTRUE < 0)]
     }
     if(raw_treat < 0){
-      diff = diff(c(max(temp$obs_cors$ES), raw_treat))
-      mostextreme = temp$obs_cors$cov[which(temp$obs_cors$ES == max(temp$obs_cors$ES))]
+      diff = diff(c(max(temp$obs_cors$ESTRUE), raw_treat))
+      mostextreme = temp$obs_cors$cov[which(temp$obs_cors$ESTRUE == max(temp$obs_cors$ESTRUE))]
     } else{
-      diff = diff(c(min(temp$obs_cors$ES), raw_treat))
-      mostextreme = temp$obs_cors$cov[which(temp$obs_cors$ES == min(temp$obs_cors$ES))]
+      diff = diff(c(min(temp$obs_cors$ESTRUE), raw_treat))
+      mostextreme = temp$obs_cors$cov[which(temp$obs_cors$ESTRUE == min(temp$obs_cors$ESTRUE))]
     }
     percent_reduced = abs(round(diff/raw_treat*100))
     text = paste0("The sign of the estimated effect is expected to be robust to unobserved confounders with strengths of associations with the treatment indicator and outcome that are seen in ", nochange, " of the ", total, " observed confounders. In the most extreme observed case, ", mostextreme, ", the estimated effect size is ", percent_reduced, " percent of the original, but in the opposite direction. The sign of the estimate would not be expected to be preserved for unobserved confounders that have the same strength of association with the treatment indicator and outcome as ", paste(changes, collapse=", "), ".")
@@ -51,7 +51,7 @@ summary.ov <- function(r1, sig_level=0.05){
   # Pvalue Size cases
   pvals=rep(NA, nrow(temp$obs_cors))
   for(i in 1:nrow(temp$obs_cors)){
-    nearest.idx <- which.min(colSums((t(temp$r1_df[,c("es_grid", "rho_grid")]) - c(temp$obs_cors$ES[i] , temp$obs_cors$Cor_Outcome[i]))^2))
+    nearest.idx <- which.min(colSums((t(temp$r1_df[,c("es_grid", "rho_grid")]) - c(temp$obs_cors$ESTRUE[i] , temp$obs_cors$Cor_Outcome[i]))^2))
     pvals[i] = temp$r1_df$p_val[nearest.idx]
   }
   if(raw_pval < sig_level & all(pvals < sig_level)){
