6d3b02ceee6899304117fad599d40f6d7791ceb5
Pane, Joseph <josephp@rand.org>
2020-04-10 16:17:43 -0400

    Finalizng graphic update

--- R/plot.ov.R
+++ R/plot.ov.R
@@ -1,11 +1,8 @@
 #### Plotting Package for OV ####
-plot.ov <- function(x, col="color", print_all="all") {
+plot.ov <- function(x, col="color", print_graphic="1") {
   prep_plots = prep_for_plots(x)
 
   #### some defaults ####
-  .pardefault <- graphics::par(no.readonly=TRUE)
-  graphics::par(ask = TRUE)
-
   plot_1 = es_plot(prep=prep_plots, col=col)
   plot_2 = pval_point_plot(prep=prep_plots, col=col)
   plot_3 = es_point_plot(prep=prep_plots,col=col)
@@ -17,30 +14,19 @@ plot.ov <- function(x, col="color", print_all="all") {
   plot_3 <- ggplot2::ggplot_gtable(ggplot2::ggplot_build(plot_3))
   plot_3$layout$clip[plot_3$layout$name == "panel"] <- "off"
 
-  if(print_all == "all"){
+  # .pardefault <- graphics::par(no.readonly=TRUE)
+  if(print_graphic == "1"){
     grid::grid.draw(plot_1)
+  } else if(print_graphic == "2"){
     grid::grid.draw(plot_2)
     if(prep_plots$text_high!=""){print(prep_plots$text_high)}
     if(prep_plots$text_high_es!=""){print(prep_plots$text_high_es)}
-    grid::grid.draw(plot_3)
-    if(prep_plots$text_high!=""){print(prep_plots$text_high)}
-    if(prep_plots$text_high_es!=""){print(prep_plots$text_high_es)}
-  }
-  else if(print_all == "1"){
-    graphics::par(.pardefault)
-    grid::grid.draw(plot_1)
-  } else if(print_all == "2"){
-    graphics::par(.pardefault)
-    grid::grid.draw(plot_2)
-    if(prep_plots$text_high!=""){print(prep_plots$text_high)}
-    if(prep_plots$text_high_es!=""){print(prep_plots$text_high_es)}
-  } else if(print_all == "3"){
-    graphics::par(.pardefault)
+  } else if(print_graphic == "3"){
     grid::grid.draw(plot_3)
     if(prep_plots$text_high!=""){print(prep_plots$text_high)}
     if(prep_plots$text_high_es!=""){print(prep_plots$text_high_es)}
   }
   # summary.ov(x)
-  graphics::par(.pardefault)
-  grDevices::palette("default")
+  # graphics::par(.pardefault)
+  # grDevices::palette("default")
 }
