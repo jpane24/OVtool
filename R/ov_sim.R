ddc44e11bded8af97380dc49690fbe2408dc4be4
Pane, Joseph <josephp@rand.org>
2020-06-30 17:17:07 -0400

    Robustly calculate es_grid and rho_grid. Give recommendations if es_grid or rho_grid were specified incorrectly

--- R/ov_sim.R
+++ R/ov_sim.R
@@ -3,7 +3,13 @@ ov_sim <- function(model_results, weight_covariates,
                    es_grid = seq(-.4, .4, by = 0.05),
                    rho_grid = seq(0, .4, by = 0.05), n_reps = 50,
                    progress = TRUE){
-  if(n_reps <= 1){"Please specify at least two (2) n_reps."}
+  if(n_reps <= 1){"Please specify at least two (2) n_reps. For example: n_reps = 50"}
+  if(any(rho_grid <0) | any(rho_grid > 1)){
+    stop("Please specify a rho grid between 0 and 1. For example: rho_grid = seq(0, .45, by = 0.05)")
+  }
+  if(any(abs(es_grid) > 1)){
+    stop("This tool only considers small to moderate correlations. An effect size of 1 corresponds to a correlation of 0.44. Please reduce the size of your grid. For example: rho_grid = seq(-.4, .4, by = 0.05)")
+  }
   tx = model_results$tx
   y = model_results$y
   data = model_results$data
@@ -23,18 +29,36 @@ ov_sim <- function(model_results, weight_covariates,
   if(!all(data[,tx] %in% c(0,1))) stop("Treatment variable `tx` must be only 0/1 values.")
 
   # determine reasonable grid to simulate over
-  jdp_test=find_esgrid(my_data = data, my_cov = cov, treatment = tx, outcome = y, my_estimand = estimand)
-  if(is.null(es_grid)){
+  jdp_test=find_esgrid(my_data = data,
+                       my_cov = cov,
+                       treatment = tx,
+                       outcome = y,
+                       my_estimand = estimand)
+  # SET final es_grid.
+  if(!is.null(es_grid)){
+    es_drop = jdp_test[which(jdp_test$ES > max(abs(es_grid))),'cov']
+    if(max(abs(es_grid)) < max(abs(jdp_test$ES))){
+      warning(paste0("You specified an effect size grid whose maximum in absolute value is less than the maximum absolute association at least one observed covariate has with the treatment indicator. The effect size grid was automatically expanded to include all weight_covariates specified in the relevant graphics. If you want the effect size grid range to remain from ", min(es_grid), " to ", max(es_grid), " then you must exclude the following variables from the weight_covariates argument: ", paste(es_drop, collapse=", "), "."))
+      es_upper = .05 * ceiling(max(jdp_test$ES)/.05)
+      es_lower = -es_upper
+      es_grid = seq(es_lower, es_upper, by=0.05)
+    }
+  } else{
     es_upper = .05 * ceiling(max(jdp_test$ES)/.05)
     es_lower = -es_upper
     es_grid = seq(es_lower, es_upper, by=0.05)
-    }
-  if(is.null(rho_grid) | (max(rho_grid) < max(jdp_test$Cor_Outcome))){
-    if(length(es_grid) > 1){
-      print("Note: The maximum rho value you specified is less than the maximum absolute correlation a covariate has with the outcome. The rho grid was automatically expanded.")
+  }
+
+  if(!is.null(rho_grid)){
+    rho_drop = jdp_test[which(jdp_test$Cor_Outcome > max(abs(rho_grid))),'cov']
+    if(max(rho_grid) < max(jdp_test$Cor_Outcome)){
+      warning(paste0("You specified a rho grid whose maximum value is less than the maximum absolute correlation at least one observed covariate has with the outcome. The rho grid was automatically expanded to include all weight_covariates specified in the relevant graphics. If you want the rho grid range to remain from ", min(rho_grid), " to ", max(rho_grid), " then you must exclude the following variables from the weight_covariates argument: ", paste(rho_drop, collapse=", "), "."))
       rho_upper = .05 * ceiling(max(jdp_test$Cor_Outcome)/.05)
       rho_grid = seq(0, rho_upper, by=0.05)
     }
+  } else{
+    rho_upper = .05 * ceiling(max(jdp_test$Cor_Outcome)/.05)
+    rho_grid = seq(0, rho_upper, by=0.05)
   }
 
   trt_effect_nodr <- matrix(0,length(es_grid),length(rho_grid))
