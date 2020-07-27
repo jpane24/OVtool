b93773bdc064722fff79facb280f1886b6248a0e
Pane, Joseph <josephp@rand.org>
2020-07-27 17:45:11 -0400

    adding add_reps function

--- R/add_reps.R
+++ R/add_reps.R
@@ -1,5 +1,5 @@
 add_reps <- function(OVtool_results, model_results, more_reps){
-  OVtool_results_new = ov_sim2(model_results = model_results,
+  OVtool_results_new = ov_sim(model_results = model_results,
                               weight_covariates = OVtool_results$cov,
                               es_grid = OVtool_results$es_grid,
                               rho_grid = OVtool_results$rho_grid,
