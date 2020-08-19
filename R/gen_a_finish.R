7ff614427ac9c77ffced5278e4b46daa55eb8af4
Pane, Joseph <josephp@rand.org>
2020-08-19 12:20:54 -0400

    ATT changing tx weights?

--- R/gen_a_finish.R
+++ R/gen_a_finish.R
@@ -36,7 +36,9 @@ gen_a_finish <- function(a_res, my_estimand){
     # a[-ind] = ((1 - a_res$pi) / (a_res$pi)) * (att_num / att_denom)
 
     # new
-    a[ind] = 1
+    a[ind] = ((1 - a_res$pi) / a_res$pi) * exp((log(a_res$pi / (1 - a_res$pi))) -
+                                                 (a_res$es^2 * (1 - 2*a_res$pi) / 2) +
+                                                 (a_res$es * U[ind]))
     a[-ind] = ((1 - a_res$pi) / a_res$pi) * exp((log(a_res$pi / (1 - a_res$pi))) -
                                                   (a_res$es^2 * (1 - 2*a_res$pi) / 2) +
                                                   (a_res$es * U[-ind]))
