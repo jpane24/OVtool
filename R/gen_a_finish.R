363d3562465ad1e9c8452b36f006f56b525085de
Pane, Joseph <josephp@rand.org>
2020-12-15 17:04:46 -0500

    fixing ATT

--- R/gen_a_finish.R
+++ R/gen_a_finish.R
@@ -1,19 +1,19 @@
 #### gen_a_finish fn ####
 gen_a_finish <- function(a_res, my_estimand){
 
-  U1 <- stats::rnorm(a_res$n1)*sqrt(a_res$ve1) + a_res$b1*a_res$Rstar_1 +
-    a_res$es*(1-a_res$pi)
-  U0 <- stats::rnorm(a_res$n0)*sqrt(a_res$ve0) + a_res$b0*a_res$Rstar_0 -
-    a_res$es*a_res$pi
-
-  U <- rep(NA, a_res$n)
-  ind <- a_res$ind
-  U[ind] <- U1
-  U[-ind] <- U0
-
-  a <- rep(NA, a_res$n)
-
   if(my_estimand == "ATE"){
+    U1 <- stats::rnorm(a_res$n1)*sqrt(a_res$ve1) + a_res$b1*a_res$Rstar_1 +
+      a_res$es*(1-a_res$pi)
+    U0 <- stats::rnorm(a_res$n0)*sqrt(a_res$ve0) + a_res$b0*a_res$Rstar_0 -
+      a_res$es*a_res$pi
+
+    U <- rep(NA, a_res$n)
+    ind <- a_res$ind
+    U[ind] <- U1
+    U[-ind] <- U0
+
+    a <- rep(NA, a_res$n)
+
     expTx = exp(-log(a_res$pi / (1 - a_res$pi)) +
                   ((a_res$es^2 * (1 - 2*a_res$pi)) / 2) -
                   (a_res$es * U[ind]))
@@ -25,23 +25,22 @@ gen_a_finish <- function(a_res, my_estimand){
     a[-ind] <- (1 - a_res$pi) * (((1 + expCntrl)) / (expCntrl))
 
   } else if(my_estimand == "ATT"){
-    # old
-    # att_num = 1 + exp((log(a_res$pi / (1 - a_res$pi)))
-    #                   - (a_res$es^2 * (1 - 2*a_res$pi) / 2) + (a_res$es * U[-ind]))
-    #
-    # att_denom = 1 + exp((-log(a_res$pi / (1 - a_res$pi)))
-    #                     + (a_res$es^2 * (1 - 2*a_res$pi) / 2) - (a_res$es * U[-ind]))
-    #
-    # a[ind] = 1
-    # a[-ind] = ((1 - a_res$pi) / (a_res$pi)) * (att_num / att_denom)
+    U0 <- stats::rnorm(a_res$n0)*sqrt(a_res$ve0) + a_res$b0*a_res$Rstar_0 -
+      a_res$es*a_res$pi
 
-    # new
+    U <- rep(NA, a_res$n)
+    ind <- a_res$ind
+    U[ind] <- 1
+    U[-ind] <- U0
+
+    a <- rep(NA, a_res$n)
+
     a[ind] = 1
     a[-ind] = ((1 - a_res$pi) / a_res$pi) * exp((log(a_res$pi / (1 - a_res$pi))) -
                                                   (a_res$es^2 * (1 - 2*a_res$pi) / 2) +
                                                   (a_res$es * U[-ind]))
 
-  } else{
+  }  else{
     stop("OVtool currently only works under the ATE or ATT setting.")
   }
 
