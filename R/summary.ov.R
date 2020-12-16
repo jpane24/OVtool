92ae5d0f4cccbc05b51e4565019f2e1c79e4245b
Pane, Joseph <josephp@rand.org>
2020-12-16 00:32:28 -0500

    update summary function with correct text

--- R/summary.ov.R
+++ R/summary.ov.R
@@ -32,39 +32,33 @@ summary.ov <- function(object, model_results, sig_level=0.05, progress = TRUE, .
                                TRUE ~ "some sign changes")
 
   if(effect_size_text == "no sign changes"){
-    diff = diff(c(min(abs(trt_effect)), abs(raw_treat)))
-    percent_reduced = abs(round(diff/raw_treat*100))
-    text = paste0("The sign of the estimated effect is expected to remain consistent when simulated unobserved confounders have the same strength of association with the treatment indicator and outcome that are seen in the observed confounders. In the most extreme observed case, the estimated effect size is reduced by ", percent_reduced, " percent.")
+    diff_which.max = which.max(abs(trt_effect - raw_treat))
+    most_extreme = trt_effect[diff_which.max]
+    text = paste0("The sign of the estimated effect is expected to remain consistent when simulated unobserved confounders have the same strength of association with the treatment indicator and outcome that are seen in the observed confounders. In the most extreme observed case, the estimated effect size shifts from ", round(raw_treat,3), " to ", round(most_extreme,3), ".")
+
   } else if(effect_size_text == "all sign changes"){
-    if(raw_treat < 0){
-      diff = diff(c(max(trt_effect), raw_treat))
-    } else{
-      diff = diff(c(min(trt_effect), raw_treat))
-    }
-    percent_reduced = abs(round(diff/raw_treat*100))
-    text = paste0("The sign of the estimated effect is *not* expected to be robust to unobserved confounders that have the same strength of association with the treatment indicator and outcome that are seen in any of the observed confounders. In the most extreme observed case, the estimated effect size is reduced by ", percent_reduced, " percent of the observed raw effect size, ", round(raw_treat,3), ".")
+    diff_which.max = which.max(abs(trt_effect - raw_treat))
+    most_extreme = trt_effect[diff_which.max]
+    text = paste0("The sign of the estimated effect is *not* expected to be robust to unobserved confounders that have the same strength of association with the treatment indicator and outcome that are seen in any of the observed confounders. In the most extreme observed case in which the sign changes, the estimated effect size shifts from ", round(raw_treat,3), " to ", round(most_extreme,3), ".")
+
   } else if(effect_size_text == "some sign changes"){
     # calculate sign changes
     if(raw_treat < 0){
       change = length(which(trt_effect >= 0))
+      which.change = which(trt_effect >= 0)
       nochange = length(which(trt_effect < 0))
       total = nrow(temp$obs_cors)
       changes = temp$obs_cors$cov[which(trt_effect >=0)]
     } else{
       change = length(which(trt_effect < 0))
+      which.change = which(trt_effect < 0)
       nochange = length(which(trt_effect >= 0))
       total = nrow(temp$obs_cors)
       changes = temp$obs_cors$cov[which(trt_effect < 0)]
     }
-    if(raw_treat < 0){
-      diff = diff(c(max(trt_effect), raw_treat))
-      mostextreme = temp$obs_cors$cov[which(trt_effect == max(trt_effect))]
-    } else{
-      diff = diff(c(min(trt_effect), raw_treat))
-      mostextreme = temp$obs_cors$cov[which(trt_effect == min(trt_effect))]
-    }
-    percent_reduced = abs(round(diff/raw_treat*100))
-    text = paste0("The sign of the estimated effect is expected to remain consistent when simulated unobserved confounders have the same strength of associations with the treatment indicator and outcome that are seen in ", nochange, " of the ", total, " observed confounders. In the most extreme observed case, ", mostextreme, ", the estimated effect size is reduced by ", percent_reduced, " percent of the observed raw effect size, ", round(raw_treat,3), ". The sign of the estimate would not be expected to be preserved for unobserved confounders that have the same strength of association with the treatment indicator and outcome as ", paste(changes, collapse=", "), ".")
+    diff_which.max = which.max(abs(trt_effect[which.change] - raw_treat))
+    most_extreme = trt_effect[which.change][diff_which.max]
+    text = paste0("The sign of the estimated effect is expected to remain consistent when simulated unobserved confounders have the same strength of associations with the treatment indicator and outcome that are seen in ", nochange, " of the ", total, " observed confounders. In the most extreme observed case in which the sign changes, the estimated effect size shifts from ", round(raw_treat,3), " to ", round(most_extreme,3), ". The sign of the estimate would not be expected to be preserved for unobserved confounders that have the same strength of association with the treatment indicator and outcome as ", paste(changes, collapse=", "), ".")
    }
 
   # Pvalue Size cases
